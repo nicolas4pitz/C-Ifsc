7531390a1a89a7461f247736f08188933a41d75e
nicolas4pitz <nicolas2004pitz@gmail.com>
2026-02-10 20:39:34 -0300

    init

--- somarNumerosInteiros.c
+++ somarNumerosInteiros.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int escolha;
+    int escolha = 0;
     int soma = 0;
 
-    while(escolha < 0){
+    while(escolha <= 0){
         printf("Escolha valores para somar ");
         scanf("%d", escolha);
-        soma = soma + escolha;
+        int somaAnterior = soma;
+        soma = escolha + somaAnterior;
     }
 
     printf("A somatoria total foi %d", escolha);
