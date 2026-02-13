07503569109f603d7eac129d7de5ce6f7c498825
nicolas4pitz <nicolas2004pitz@gmail.com>
2026-02-13 17:34:14 -0300

    feat: somar numeros

--- somarNumerosInteiros.c
+++ somarNumerosInteiros.c
@@ -3,12 +3,14 @@
 int main(){
     int escolha = 0;
     int soma = 0;
+    int count = 0;
 
     while(escolha <= 0){
         printf("Escolha valores para somar ");
-        scanf("%d", escolha);
+        scanf("%d", &escolha);
         int somaAnterior = soma;
-        soma = escolha + somaAnterior;
+        soma += escolha;
+        count++;
     }
 
     printf("A somatoria total foi %d", escolha);
